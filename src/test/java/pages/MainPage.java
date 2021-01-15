e0f0162bac6c05105895006f7f474213d94b51d2
Kristi-n-a <70910552+Kristi-n-a@users.noreply.github.com>
2021-01-15 17:53:25 +0200

    created classes

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,4 +1,43 @@
 package pages;
 
-public class MainPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class MainPage extends PageBase {
+    public MainPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = ".hide-xs.md.title-default.hydrated")
+    WebElement slogan;
+
+    @FindBy(css ="ion-buttons.select.buttons-last-slot.sc-ion-buttons-md-h.sc-ion-buttons-md-s.md.hydrated")
+    WebElement selectLang;
+
+    @FindBy(id = "ion-rb-1-lbl")
+    WebElement russianLang;
+
+    @FindBy(css = "ion-item.select-interface-option")
+    List<WebElement> langList;
+
+    @FindBy(tagName = "ion-card-title")
+    WebElement title;
+
+
+    public boolean isSloganContainsText(String text) {
+        return slogan.getText().contains(text);
+    }
+
+    public void selectRussianLanguage() {
+        selectLang.click();
+        waitUntilElementVisible(russianLang, 30);
+        langList.get(1).click();
+    }
+
+    public boolean isLanguageOnPageRussian() {
+        return title.getText().contains("Найдите самый дешевый способ добраться из города в город, сочетая самолет, поезд, автобус и совместные поездки на автомобиле");
+    }
 }
--- src/test/java/tests/MainPageTests.java
+++ src/test/java/tests/MainPageTests.java
@@ -1,6 +1,40 @@
 package tests;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.MainPage;
+
+import java.util.concurrent.TimeUnit;
+
 public class MainPageTests  extends TestBase{
+    ChromeDriver wd;
+
+    MainPage mainPage;
+
+    @BeforeMethod
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        wd.manage().window().maximize();
+        wd.get("http://test70.lowcoststrip.com/places/tabs/discover");
+    }
+
+    @Test(priority = 2, groups = {"UI"})
+
+    public void sloganValidationTest(){
+        String text = "CheapTrip. Pay less, travel more";
+        assert mainPage.isSloganContainsText(text);
+    }
+
+    @Test(priority = 1, groups = {"functional"})
+    public void changelanguageValidationTest() throws InterruptedException{
+        mainPage.selectRussianLanguage();
+        Thread.sleep(500);
+        assert mainPage.isLanguageOnPageRussian();
+    }
+
 
 
 }
